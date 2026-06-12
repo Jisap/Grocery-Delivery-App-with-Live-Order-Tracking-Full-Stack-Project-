7ff39ce887e7bc72020df703096b68f65d7ea09f
Jorge Matos <jisapjisap@gmail.com>
2026-06-12 17:37:14 +0200

    feat: implement admin product management and delivery partner dashboard modules

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,15 @@ import MyOrders from "./pages/MyOrders"
 import OrderTracking from "./pages/OrderTracking"
 import Adressess from "./pages/Adressess"
 import ProtectedRoute from "./components/ProtectedRoute"
+import AdminLayout from "./pages/admin/AdminLayout"
+import AdminDashboard from "./pages/admin/AdminDashboard"
+import AdminProducts from "./pages/admin/AdminProducts"
+import AdminProductForm from "./pages/admin/AdminProductForm"
+import AdminOrders from "./pages/admin/AdminOrders"
+import AdminDeliveryPartners from "./pages/admin/AdminDeliveryPartners"
+import DeliveryLogin from "./pages/delivery/DeliveryLogin"
+import DeliveryLayout from "./pages/delivery/DeliveryLayout"
+import DeliveryDashboard from "./pages/delivery/DeliveryDashboard"
 
 
 const App = () => {
@@ -47,8 +56,27 @@ const App = () => {
             <Route path="orders/:id" element={<OrderTracking />} />
             <Route path="addresses" element={<Adressess />} />
           </Route>
-
         </Route>
+
+        {/* Admin pages */}
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<AdminDashboard />} />
+          <Route path="products" element={<AdminProducts />} />
+          <Route path="products/new" element={<AdminProductForm />} />
+          <Route path="products/edit" element={<AdminProductForm />} />
+          <Route path="products/:id/edit" element={<AdminProductForm />} />
+          <Route path="orders" element={<AdminOrders />} />
+          <Route path="delivery-partners" element={<AdminDeliveryPartners />} />
+        </Route>
+
+        {/* Delivery partner pages */}
+        <Route path="/delivery/login" element={<DeliveryLogin />} />
+        <Route path="/delivery" element={<DeliveryLayout />}>
+          <Route index element={<DeliveryDashboard />} />
+        </Route>
+
+
+
       </Routes>
     </>
   )
